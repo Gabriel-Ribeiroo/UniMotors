75745a82b8f417d2b9043e31ae699c5b18d536f3
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2026-05-09 22:05:54 -0300

    feat: add keydown listener only when sidebar is open

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,19 +1,19 @@
+import { handleKeydown } from "./onKeydown.js"
+
 const sidebar = document.querySelector("[data-js='sidebar']")
 
 export function openSidebar() {
 	sidebar.classList.remove("hidden")
+	document.addEventListener("keydown", closeSidebarOnEscPress)
 }
 
 export function closeSidebar() {
 	sidebar.classList.add("hidden")
+	document.removeEventListener("keydown", closeSidebarOnEscPress)
 }
 
 export function closeSidebarOnEscPress(event) {
-	if (event.key.toLowerCase() !== "escape") return 
-
-	if (!sidebar.classList.contains("hidden")) {
-		closeSidebar()
-	}
+	handleKeydown({ event, key: "Escape", onKeydown: closeSidebar })
 }
 
 const sidebarOpenButton = document.querySelector("[data-js='sidebar-open']")
@@ -21,5 +21,3 @@ sidebarOpenButton.addEventListener("click", openSidebar)
 
 const sidebarCloseButtons = document.querySelectorAll("[data-js='sidebar-close']")
 sidebarCloseButtons.forEach(close => close.addEventListener("click", closeSidebar))
-
-document.addEventListener("keydown", closeSidebarOnEscPress)
